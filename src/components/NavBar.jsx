36a9248a6270500b502b7f9a9da4c37c9dda5375
Pravaritti <pravaritti.1995@gmail.com>
2025-07-20 18:00:55 +0530

    refactor and put checks in login feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     const user= useSelector((store) => store.user);
@@ -8,7 +9,7 @@ const NavBar = () => {
     <div>
         <div className="navbar bg-base-300 shadow-sm justify-between ">
             <div >
-                <a className="btn btn-ghost text-xl">DevConnect</a>
+                <Link to="/" className="btn btn-ghost text-xl">DevConnect</Link>
             </div>
             {user && <div className="flex-none gap-2 flex">
                 <div className="form-control px-4">Welcome, {user.firstName}</div>
@@ -24,10 +25,10 @@ const NavBar = () => {
                     tabIndex={0}
                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                     <li>
-                    <a className="justify-between">
+                    <Link to="/profile" className="justify-between">
                         Profile
                         <span className="badge">New</span>
-                    </a>
+                    </Link>
                     </li>
                     <li><a>Settings</a></li>
                     <li><a>Logout</a></li>
