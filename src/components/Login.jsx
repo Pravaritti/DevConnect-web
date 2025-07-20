9bb317ed9e2af115ade1c51b0c741596ba1aed8b
Pravaritti <pravaritti.1995@gmail.com>
2025-07-20 11:08:01 +0530

    create login page and redirect it to feed page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux'; 
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
+  const [emailId, setEmailId]= useState("elon@musk.com");
+  const [password, setPassword]= useState("Elon@123");
+  const dispatch= useDispatch();
+  const navigate= useNavigate();
+
+  const handleLogin= async () => {
+    try{
+      const res= await axios.post(
+        BASE_URL+"/login", 
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data));
+      return navigate("/");
+
+    }catch(err){
+      console.error(err);
+    }
+  };
+
   return (
-    <div>Login</div>
+    <div className='flex justify-center my-20'>
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input 
+                type="text" 
+                value={emailId}
+                className="input input-bordered w-full max-w-xs" 
+                placeholder="Type here"
+                onChange={(e)=> setEmailId(e.target.value)} 
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input 
+                type="text" 
+                value={password}
+                className="input" 
+                placeholder="Type here" 
+                onChange={(e)=> setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const NavBar = () => {
+    const user= useSelector((store) => store.user);
+
   return (
     <div>
-        <div className="navbar bg-base-300 shadow-sm justify-between">
+        <div className="navbar bg-base-300 shadow-sm justify-between ">
             <div >
                 <a className="btn btn-ghost text-xl">DevConnect</a>
             </div>
-            <div className="flex-none gap-2">
+            {user && <div className="flex-none gap-2 flex">
+                <div className="form-control px-4">Welcome, {user.firstName}</div>
                 <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                     <div className="w-10 rounded-full">
                     <img
-                        alt="Tailwind CSS Navbar component"
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        alt="user photo" 
+                        src={user.photoUrl} />
                     </div>
                 </div>
                 <ul
@@ -29,7 +33,7 @@ const NavBar = () => {
                     <li><a>Logout</a></li>
                 </ul>
                 </div>
-            </div>
+            </div>}
         </div>
     </div>
   );
