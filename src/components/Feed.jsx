2ba7c846f4f302c08661b13374a3d00902225fb2
Pravaritti <pravaritti.1995@gmail.com>
2025-07-20 22:41:02 +0530

    create Feed component and feedSlice in redux store

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { BASE_URL } from '../utils/constants';
+import { addFeed } from '../utils/feedSlice';
 
 export const Feed = () => {
+  const feed= useSelector((store) => store.feed);
+  const dispatch= useDispatch();
+
+  const getFeed= async () => {
+    if(feed) return;
+    try {
+      const res= await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch(err){}
+  };
+
+  useEffect(()=> {
+    getFeed();
+  }, []);
+
   return (
     <div>Feed</div>
   )
