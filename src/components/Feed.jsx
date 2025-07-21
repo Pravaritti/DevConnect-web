03a50cac95a02bb89fb90ba638cd87ca696331e1
Pravaritti <pravaritti.1995@gmail.com>
2025-07-21 14:33:45 +0530

    create user card on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,26 +3,35 @@ import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { BASE_URL } from '../utils/constants';
 import { addFeed } from '../utils/feedSlice';
+import UserCard from './UserCard';
 
 export const Feed = () => {
   const feed= useSelector((store) => store.feed);
   const dispatch= useDispatch();
-
+  
   const getFeed= async () => {
     if(feed) return;
     try {
       const res= await axios.get(BASE_URL + "/feed", {
         withCredentials: true,
       });
-      dispatch(addFeed(res.data));
-    } catch(err){}
+      console.log(res.data)
+      dispatch(addFeed(res?.data));
+    } catch(err){
+      console.error("Error fetching feed:", err);
+    }
   };
+  
 
   useEffect(()=> {
     getFeed();
   }, []);
-
+  
   return (
-    <div>Feed</div>
+    feed && (
+      <div className='flex justify-center my-10'>
+        <UserCard user={feed[0]}/>
+      </div>
+    )
   )
 }
